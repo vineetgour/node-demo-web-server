b7c0c4dfc6035e6d492197854c6611c4998582a4
unknown <vineetgaur97@gmail.com>
2019-09-10 16:11:05 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () =>{
+  return new Date().getFullYear();
+});
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err){
+      console.log('Unable to append');
+    }
+  });
+  next();
+});
+// app.use((req, res, next) => {
+//  res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+  //res.send('Hello Express!');
+  res.render('home.hbs', {
+    pageTitle: 'About Page',
+    welcomeMessage: 'Welcome To My Website',
+  });
+});
+
+app.get('/about',(req,res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+app.get('/bad',(req,res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(3000);
